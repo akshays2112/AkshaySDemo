ed943f0e9c9ca44066f6f63d1c20f23998db3f22
Akshay Srinivasan <akshays2112@gmail.com>
2020-12-12 21:40:17 +0530

    Small fixes to get rid of button click.

--- EpicAkSBlazorSTwitterClient/ClassDefs.cs
+++ EpicAkSBlazorSTwitterClient/ClassDefs.cs
@@ -11,6 +11,7 @@ namespace EpicAkSBlazorSTwitterClient
         public static string RedirectUri;
         public static string TwitterApiKey = "ngtvtlOEDXogCOvgxskFAZckI";
         public static string TwitterApiSecret = "6szb2CphSAK4wMXm8fW8UO8UnEY3Ql0WEANihtCvDt2GJVeJfb";
+        public static string TwitterAccessToken = string.Empty;
         public static LocalAuthenticationRequestStore _myAuthRequestStore = new LocalAuthenticationRequestStore();
         public static ITwitterCredentials UserCredentials;
 
