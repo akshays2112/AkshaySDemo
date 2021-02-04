0855bdcf98ed101285e948e801087d0ee0565b37
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2021-02-04 21:02:53 +0530

    Getting things working again.

--- EpicAkSAuthenticationPages/Pages/Index.cshtml.cs
+++ EpicAkSAuthenticationPages/Pages/Index.cshtml.cs
@@ -16,11 +16,11 @@ namespace EpicAkSAuthenticationPages.Pages
 
         public void OnGet()
         {
-            string catTempSessionUID = ClientAppToken.SpotifyApi.GenerateClientAppTokenForSpotifyApi(clientId, clientSecret).CATTempSessionUID;
-            CookieOptions cookieOptions = new CookieOptions();
-            cookieOptions.Expires = DateTime.Now.AddMinutes(30);
-            Response.Cookies.Append("catTempSessionUID", catTempSessionUID, cookieOptions);
-            Response.Redirect($"/SpotifyAuth");
+            //string catTempSessionUID = ClientAppToken.SpotifyApi.GenerateClientAppTokenForSpotifyApi(clientId, clientSecret).CATTempSessionUID;
+            //CookieOptions cookieOptions = new CookieOptions();
+            //cookieOptions.Expires = DateTime.Now.AddMinutes(30);
+            //Response.Cookies.Append("catTempSessionUID", catTempSessionUID, cookieOptions);
+            //Response.Redirect($"/SpotifyAuth");
         }
     }
 }
